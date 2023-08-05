c61f7245e1f912b759ec3925a09b0f022d34298d
Alvin Januar <alvin.januar@ecloudvalley.com>
2023-08-05 23:53:25 +0800

    feat: connect aws freebies

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { exampleRouter } from "~/server/api/routers/example";
+import { awsRouter } from "~/server/api/routers/aws";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  aws: awsRouter
 });
 
 // export type definition of API
---
+++ src/server/api/routers/aws.ts
@@ -0,0 +1,67 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const awsRouter = createTRPCRouter({
+  freeTierLimit: publicProcedure
+    .input(z.object({ size: z.number().default(40) }))
+    .query(async ({ input }) => {
+      const params = {
+        "item.directoryId": "free-tier-products",
+        "sort_by":"item.additionalFields.SortRank",
+        "sort_order":"asc",
+        "size": input.size + "",
+        "item.locale":"en_US",
+      };
+      const url = "https://aws.amazon.com/api/dirs/items/search?" + new URLSearchParams([
+        ["tags.id", "free-tier-products#tier#always-free"],
+        ["tags.id", "!free-tier-products#test#test"],
+        ...Object.entries(params)
+      ]).toString();
+      const test = await fetch(url);
+      const response = await test.json() as FreeTierLimitResponse;
+      return { ...response };
+    }),
+});
+
+type FreeTierLimitResponse = {
+  items: ItemType[],
+  metadata: {
+    count: number,
+    totalHits: number
+  },
+  fieldTypes: FieldType
+}
+
+export type ItemType = {
+  item: {
+    id: string,
+    locale: string,
+    directoryId: string,
+    name: string,
+    author: string,
+    lastUpdatedBy: string,
+    dateCreated: string,
+    additionalFields: FieldType,
+  }
+}
+
+export type FieldType = {
+  serviceType: string,
+  new: string,
+  imageSrcUrl: string,
+  backDescriptionExtra: string,
+  backDescription: string,
+  cardColor: string,
+  serviceName: string,
+  campaignUrl: string,
+  frontCtaText: string,
+  SortRank: string,
+  frontCtaUrl: string,
+  calloutSmall: string,
+  offerDetail: string,
+  backCtaText: string,
+  calloutLarge: string,
+  backCtaUrl: string,
+  frontDescription: string,
+  category: string
+}
